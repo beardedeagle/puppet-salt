00570c040b36348540f0714f0577c286ca703d56
Randy Thompson <rlthompson@godaddy.com>
2015-12-05 01:28:57 -0600

    trying to fix duplicate repo declaration
    trying to fix duplicate repo declaration

--- manifests/master/repo.pp
+++ manifests/master/repo.pp
@@ -1,8 +1,8 @@
 # this class is used to ensure the saltstack repo is present before install
 class salt::master::repo (
-  $repo_file_manage                       = $salt::params::repo_file_manage,
-  $repo_file                              = $salt::params::repo_file,
-  $repo_file_template                     = $salt::params::repo_file_template,)
+  $repo_file_manage                       = $salt::master::repo_file_manage,
+  $repo_file                              = $salt::master::repo_file,
+  $repo_file_template                     = $salt::master::repo_file_template,)
 inherits salt::master {
   # installs the master config file defined in salt::params
   file { $repo_file:
--- manifests/minion/repo.pp
+++ manifests/minion/repo.pp
@@ -1,8 +1,8 @@
 # this class is used to ensure the saltstack repo is present before install
 class salt::minion::repo (
-  $repo_file_manage                       = $salt::params::repo_file_manage,
-  $repo_file                              = $salt::params::repo_file,
-  $repo_file_template                     = $salt::params::repo_file_template,)
+  $repo_file_manage                       = $salt::minion::repo_file_manage,
+  $repo_file                              = $salt::minion::repo_file,
+  $repo_file_template                     = $salt::minion::repo_file_template,)
 inherits salt::minion {
   # installs the minion config file defined in salt::params
   file { $repo_file:
